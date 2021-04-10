ce156a0872abd85ce9d65e05bdf6cb71927743fc
cheddy1 <elcman965@gmai.com>
2021-04-10 15:27:56 -0500

    updates

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
  
  var client_id = '032686f37ffd4b5a916228f098a53930'; // Your client id
  var client_secret = '961ab4181074461aa727eb7f30e4fca1'; // Your secret
- var redirect_uri = 'http://localhost:8888/location'; // Your redirect uri
+ var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
  
  /**
   * Generates a random string containing numbers and letters
@@ -118,30 +118,8 @@
      });
    }
  });
- 
- app.get('/refresh_token', function(req, res) {
- 
-   // requesting access token from refresh token
-   var refresh_token = req.query.refresh_token;
-   var authOptions = {
-     url: 'https://accounts.spotify.com/api/token',
-     headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-     form: {
-       grant_type: 'refresh_token',
-       refresh_token: refresh_token
-     },
-     json: true
-   };
- 
-   request.post(authOptions, function(error, response, body) {
-     if (!error && response.statusCode === 200) {
-       var access_token = body.access_token;
-       res.send({
-         'access_token': access_token
-       });
-     }
-   });
- });
+  
+
  
  console.log('Listening on 8888');
  app.listen(8888);
