660757e472ad35461d89fda7723849e3daa55104
cheddy1 <elcman965@gmai.com>
2021-04-10 23:58:43 -0500

    formatting

--- public/map.js
+++ public/map.js
@@ -9,7 +9,18 @@ function initMap() {
   let latlng = new google.maps.LatLng(-34.397, 150.644);
   var myMarkerOptions = {
     position: latlng,
-    map: map
+    map: map, 
  }
- var myMarker = new google.maps.Marker(myMarkerOptions);
+ var infowindow = new google.maps.InfoWindow();
+ infowindow.setContent('<b>Faith</b><br>The Weeknd<br>');
+ var marker = new google.maps.Marker(myMarkerOptions);
+  marker.addListener('mouseover', function() {
+    infowindow.open(map, this);
+  });
+
+  // assuming you also want to hide the infowindow when user mouses-out
+  marker.addListener('mouseout', function() {
+    infowindow.close();
+  });
+ 
 }
