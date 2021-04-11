62df7774934bdd9b62c7009008c23f217b1445ee
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-04-11 12:19:20 -0500

    remove client secrets

--- app.js
+++ app.js
@@ -13,8 +13,8 @@
  var querystring = require('querystring');
  var cookieParser = require('cookie-parser');
  
- var client_id = '032686f37ffd4b5a916228f098a53930'; // Your client id
- var client_secret = '961ab4181074461aa727eb7f30e4fca1'; // Your secret
+ var client_id = ''; // Your client id
+ var client_secret = ''; // Your secret
  var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
  
  /**
